ab3cc5426a2510d0f26986f2ffc3f8a0803fc39a
Kev <kevin.estrada1@udea.edu.co>
2024-10-08 01:44:34 -0500

    Docker Actualizado

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,27 @@
-FROM openjdk:17-jdk-slim
+# Usa una imagen de Maven con OpenJDK (Eclipse Temurin) para construir el proyecto
+FROM maven:3.8.8-eclipse-temurin-17-alpine AS build
+
+# Establece el directorio de trabajo para la fase de construcción
+WORKDIR /app
+
+# Copia el archivo POM y el código fuente al contenedor
+COPY pom.xml ./
+COPY src ./src
+
+# Ejecuta la compilación del proyecto utilizando Maven
+RUN mvn clean package -DskipTests
+
+# Fase de producción con OpenJDK
+FROM eclipse-temurin:17-jdk-alpine
 
 # Establece el directorio de trabajo en el contenedor
 WORKDIR /app
 
-# Copia el archivo JAR generado al contenedor
-COPY target/payments.jar /app/modulo-pagos.jar
+# Copia el JAR generado en la fase anterior
+COPY --from=build /app/target/payments.jar /app/modulo-pagos.jar
 
+# Expone el puerto de la aplicación
 EXPOSE 8081
 
+# Comando de inicio para la aplicación
 ENTRYPOINT ["java", "-jar", "/app/modulo-pagos.jar"]
--- init-scripts/data.sql
+++ init-scripts/data.sql
@@ -1,7 +1,39 @@
--- Crear métodos de pago
-INSERT INTO payment_method (id, name) VALUES (1, 'Credit Card');
-INSERT INTO payment_method (id, name) VALUES (2, 'PayPal');
+CREATE TABLE IF NOT EXISTS payment_method (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
 
--- Crear gateways de pago
-INSERT INTO gateway_payment (id, name) VALUES (1, 'Stripe');
-INSERT INTO gateway_payment (id, name) VALUES (2, 'PayU');
+INSERT INTO payment_method (name) VALUES ('Credit Card');
+INSERT INTO payment_method (name) VALUES ('PayPal');
+INSERT INTO payment_method (name) VALUES ('Debit Card');
+
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY
+);
+
+INSERT INTO "user" (id) VALUES (1);
+INSERT INTO "user" (id) VALUES (2);
+INSERT INTO "user" (id) VALUES (3);
+
+CREATE TABLE IF NOT EXISTS gateway_payment (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO gateway_payment (name) VALUES ('Stripe');
+INSERT INTO gateway_payment (name) VALUES ('PayU');
+INSERT INTO gateway_payment (name) VALUES ('Mercado Pago');
+
+CREATE TABLE IF NOT EXISTS booking (
+    id SERIAL PRIMARY KEY,
+    is_paid BOOLEAN NOT NULL DEFAULT FALSE,
+    price FLOAT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
+
+INSERT INTO booking (is_paid, price, user_id) VALUES (FALSE, 100.50, 1);
+INSERT INTO booking (is_paid, price, user_id) VALUES (FALSE, 200.75, 2);
+INSERT INTO booking (is_paid, price, user_id) VALUES (FALSE, 300.00, 3);
+
+
